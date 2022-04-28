442d470dbf8425f3f6458f699381260b651ad52d
จิตรกร ศรีศิลป์ <1160304620132@mail.rmutt.ac.th>
2022-04-28 09:45:24 +0700

    Uploaded 28/4/2022

---
+++ PingCheckServerDown/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PingCheckServerDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelServer = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelServer.Location = new System.Drawing.Point(223, 146);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(340, 33);
+            this.labelServer.TabIndex = 0;
+            this.labelServer.Text = "Server Down Ping Check";
+            this.labelServer.UseMnemonic = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(317, 207);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(151, 72);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.labelServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ PingCheckServerDown/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PingCheckServerDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string DateNow = DateTime.Now.ToString();
+            string[] GetText;
+            string[] GetRobin;
+            List<string> DataList = new List<string>();
+            try
+            {
+                GetText = System.IO.File.ReadAllLines(ConfigurationManager.AppSettings.Get("ReadFilePath"));
+                GetRobin = System.IO.File.ReadAllLines(ConfigurationManager.AppSettings.Get("ReadFileRobin"));
+                DataList = GetText.ToList();
+                var GetTextOffline = DataList.Where(stringToCheck => stringToCheck.Contains("Offline"));
+                if (GetTextOffline.Count() == 0)
+                {
+                    LineNotify("แจ้งเตือนสถานะ EDS System" + "\n" + "ประจำวันที่: " + DateNow + "\n" + "\n" + "ระบบทำงานเป็นปกติ" + "\n" + "Robin Updated: " + GetRobin[2]);
+                    Application.Exit();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            catch (Exception Error)
+            {
+                //Create Log File
+                string LogFilePath = ConfigurationManager.AppSettings.Get("LogFile");
+                using (FileStream FileLog = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
+                using (StreamWriter WriteFile = new StreamWriter(FileLog))
+                {
+                    WriteFile.WriteLine(DateTime.Now);
+                    WriteFile.WriteLine(Error.Message);
+                    WriteFile.WriteLine("{0} Exception caught.", Error);
+                    WriteFile.WriteLine("-----------------------------------------------------------------");
+                }
+            }
+        }
+        private void LineNotify(string msg)
+        {
+            string token = ConfigurationManager.AppSettings.Get("Token");
+            try
+            {
+                //Initial LINE API
+                var request = (HttpWebRequest)WebRequest.Create("https://notify-api.line.me/api/notify");
+                var postData = string.Format("message={0}", msg);
+                var data = Encoding.UTF8.GetBytes(postData);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                request.Headers.Add("Authorization", "Bearer " + token);
+                using (var stream = request.GetRequestStream()) stream.Write(data, 0, data.Length);
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                //
+            }
+            catch (Exception Error)
+            {
+                //Create Log File
+                string LogFilePath = ConfigurationManager.AppSettings.Get("LogFile");
+                using (FileStream FileLog = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
+                using (StreamWriter WriteFile = new StreamWriter(FileLog))
+                {
+                    WriteFile.WriteLine(DateTime.Now);
+                    WriteFile.WriteLine(Error.Message);
+                    WriteFile.WriteLine("{0} Exception caught.", Error);
+                    WriteFile.WriteLine("-----------------------------------------------------------------");
+                }
+            }
+        }
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
